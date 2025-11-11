1d32a6b482ddc50f8018957466544fe62806fff1
1mctrl <kefirzefir523@gmail.com>
2025-11-11 16:02:14 +0000

    update?

--- target/debug/deps/structs-d7e58a8c3a2b90b8.d
+++
@@ -1,5 +0,0 @@
-/home/icon/Rust-prj/target/debug/deps/structs-d7e58a8c3a2b90b8.d: structs/src/main.rs
-
-/home/icon/Rust-prj/target/debug/deps/structs-d7e58a8c3a2b90b8: structs/src/main.rs
-
-structs/src/main.rs:
--- target/debug/structs.d
+++
@@ -1,1 +0,0 @@
-/home/icon/Rust-prj/target/debug/structs: /home/icon/Rust-prj/structs/src/main.rs
